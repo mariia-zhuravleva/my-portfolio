b70046ef02015d7adae8f4624d27d953b6d849e9
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-22 11:30:21 -0500

    create ProjectsIndex component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 
 // components
 import NavBar from './components/NavBar/NavBar'
+import About from './pages/About/About'
+import ProjectsIndex from './pages/ProjectsIndex/ProjectsIndex'
 
 
 import './App.css'
@@ -14,7 +16,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path='/about' element={<About />}/>
-        <Route path='/projects' element={<Projects />}/>
+        <Route path='/projects' element={<ProjectsIndex />}/>
         {/* <Route path='/projects/:projectId' element={<ProjectShow />}/> */}
         <Route path='/contact' element={<Contact />}/>
       </Routes>
