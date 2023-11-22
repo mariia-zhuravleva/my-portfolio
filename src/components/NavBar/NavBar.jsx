7d15bbb9ed8b8e2c77fd688a4aa9ee690d9f8a44
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-22 11:10:23 -0500

    create navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+// npm modules
+import { NavLink } from 'react-router-dom'
+
+// css
+import styles from './NavBar.module.css'
+
+const NavBar = () => {
+
+  return (
+    <nav className={`${styles.container}`}>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>     
+    </nav>
+  )
+}
+
+export default NavBar
