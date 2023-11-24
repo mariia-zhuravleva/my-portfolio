149601b3f798d328e02ecd002e13b67a205f2270
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-24 12:27:12 -0500

    add css style to navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,17 @@
+import styles from './Home.module.css'
+// import background from "../../assets/img/background.png"
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={`${styles.homeContainer}`}>
+      <div className={`${styles.homePhraseContainer}`}>
+        {/* <img src={background} alt="image" /> */}
+        <p className={`${styles.homePhrase}`}>
+          Blending <span className={`${styles.homeSpan}`}>Aesthetics</span> and Seamless <span className={`${styles.homeSpan} ${styles.secondLine}`}>Functionality</span>
+        </p>
+      </div>
+      {/* <div className={`${styles.homeBackground}`}>
+      </div> */}
     </div>
   )
 }
