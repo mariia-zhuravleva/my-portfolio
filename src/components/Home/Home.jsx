3f5d92322a3e2f57d6786e8d880d807fc84ec3e1
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-24 16:07:47 -0500

    add css style to landing page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,26 @@
 import styles from './Home.module.css'
-// import background from "../../assets/img/background.png"
+import arrowDown from "../../assets/img/arrowDown.png"
 
 const Home = () => {
   return (
     <div className={`${styles.homeContainer}`}>
+      <div className={styles.topRight}>
+        <p>Committed to creating sophisticated digital experiences that captivate users, ensuring a smooth and intuitive interaction.</p>
+      </div>
       <div className={`${styles.homePhraseContainer}`}>
-        {/* <img src={background} alt="image" /> */}
         <p className={`${styles.homePhrase}`}>
           Blending <span className={`${styles.homeSpan}`}>Aesthetics</span> and Seamless <span className={`${styles.homeSpan} ${styles.secondLine}`}>Functionality</span>
         </p>
       </div>
-      {/* <div className={`${styles.homeBackground}`}>
-      </div> */}
+      <div className={styles.bottomLeft}>
+        <p className={styles.name}>Mariia Zhuravleva</p>
+        <p>Software Engineer / Full Stack Web Developer </p>
+        <div className={styles.arrowContainer}>
+          <a href="#end-of-page" className={styles.arrowDown}>
+            <img src={arrowDown} alt="arrow-down" />
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,14 +7,24 @@ import styles from './NavBar.module.css'
 const NavBar = () => {
 
   return (
-    <nav className={`${styles.container}`}>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/projects">Projects</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-      </ul>     
-    </nav>
+    <div className={`${styles.navbarContainer}`}>
+      <div className={`${styles.sideContainer}`}>
+        <ul className={`${styles.verticalList}`}>
+          <hr className={`${styles.sideLine}`} />    
+          <li><NavLink to="https://www.linkedin.com/in/mariia-zhuravleva/">LI</NavLink></li>
+          <li><NavLink to="https://github.com/mariia-zhuravleva">GitHub</NavLink></li>
+          <li><NavLink to="https://www.instagram.com/kalininaaa/">Inst</NavLink></li>
+        </ul> 
+      </div>
+      <nav className={`${styles.container}`}>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/projects">Projects</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul> 
+      </nav>
+    </div>
   )
 }
 
