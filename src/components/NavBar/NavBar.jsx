026bf31593495411a7f27b23672416f3eb719b80
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-27 13:08:14 -0500

    add css style to navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,25 @@
 // npm modules
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom'
 
 // css
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0
+      setScrolled(isScrolled)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
     <div className={`${styles.navbarContainer}`}>
