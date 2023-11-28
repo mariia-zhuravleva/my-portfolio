b5cf30424aea4eab7f43fb4b7fadfb8e8c18c49c
Maria Zhuravleva <contact.mzhuravleva@gmail.com>
2023-11-28 09:03:17 -0500

    add css style to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 
@@ -20,7 +21,6 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
         <Route path='/projects' element={<ProjectsIndex />}/>
-        {/* <Route path='/projects/:projectId' element={<ProjectShow />}/> */}
         <Route path='/contact' element={<Contact />}/>
       </Routes>
     </>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Home.module.css'
 import arrowDown from "../../assets/img/arrowDown.png"
+import About from '../About/About'
 
 const Home = () => {
   return (
-    <div className={`${styles.homeContainer}`}>
-      <div className={styles.topRight}>
-        <p>Committed to creating sophisticated digital experiences that captivate users, ensuring a smooth and intuitive interaction.</p>
-      </div>
-      <div className={`${styles.homePhraseContainer}`}>
-        <p className={`${styles.homePhrase}`}>
-          Blending <span className={`${styles.homeSpan}`}>Aesthetics</span> and Seamless <span className={`${styles.homeSpan} ${styles.secondLine}`}>Functionality</span>
-        </p>
-      </div>
-      <div className={styles.bottomLeft}>
-        <p className={styles.name}>Mariia Zhuravleva</p>
-        <p>Software Engineer / Full Stack Web Developer </p>
-        <div className={styles.arrowContainer}>
-          <a href="#end-of-page" className={styles.arrowDown}>
-            <img src={arrowDown} alt="arrow-down" />
-          </a>
+    <div className={`${styles.bigContainer}`}>
+      <div className={`${styles.homeContainer}`}>
+        <div className={styles.topRight}>
+          <p>Committed to creating sophisticated digital experiences that captivate users, ensuring a smooth and intuitive interaction</p>
         </div>
+        <div className={`${styles.homePhraseContainer}`}>
+          <p className={`${styles.homePhrase}`}>
+            Blending <span className={`${styles.homeSpan}`}>Aesthetics</span> and Seamless <span className={`${styles.homeSpan} ${styles.secondLine}`}>Functionality</span>
+          </p>
+        </div>
+        <div className={styles.bottomLeft}>
+          <p className={styles.name}>Mariia Zhuravleva</p>
+          <p>Software Engineer / Full Stack Web Developer </p>
+        </div>
+      </div>
+      <div className={styles.arrowContainer}>
+        <Link to="/about" className={styles.arrowDownLink}>
+          <div className={styles.arrowDown}>
+            <img src={arrowDown} alt="arrow-down" />
+          </div>
+        </Link>
+      </div>
+      <div className={`${styles.bigAboutContainer}`}>
+        <About />
       </div>
     </div>
   )
